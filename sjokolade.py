732ef8aca19d8a7f45898cd299e0c720f6dad847
MikaelEmelin <114730914+MikaelEmelin@users.noreply.github.com>
2022-12-12 12:10:02 +0100

    Update sjokolade.py

--- sjokolade.py
+++ sjokolade.py
@@ -8,4 +8,11 @@ Ellers print tallet (i)
 """
 
 for i in range(1, 101):
-    print(i)
+        if i % 3 == 0 and i % 5 == 0:
+            print("SjokoLade")
+        elif i % 3 == 0:
+            print("Sjoko")
+        elif i % 5 == 0:
+            print("Lade")
+        else:
+            print(i)
